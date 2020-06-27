8f70c2b9957de6adf1e8d6ac54e60ca4e6968a53
saraalqassar250 <66700227+saraalqassar250@users.noreply.github.com>
2020-06-27 20:48:59 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,25 +1,38 @@
 // السؤال الأول
 // 1
+const name=" sara";
 
 // 2
-
+let number= 15;
 // 3 and 4
-
+console.log( name + " " +  number)
 // 5 and 6
-
-// -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
+number=number*5;
+console.log(number*5)
+// -.-.:-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies= ["photography","art", "cooking"]
+console.log(hobbies)
 // 2
-
+hobbies.push(number)
+hobbies.pop();
 // 3
-
+console.log(hobbies.length)
 // 4
+let student={
+    name:"sara",
+    path:"web",
+    lang:"css"
+}
+console.log(student.path)
 
 // 5
+student.ta=["rahaf", "moudhi"]
 
+    
+console.log(student.ta[1])
 // 6
 
 // 7
